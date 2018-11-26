95335a534634ea35e1c16ea80734a95237172380
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2018-11-26 12:29:12 -0200

    fiz o setup básico do devise. o próximo passo será criar o model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -32,18 +32,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
+  
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+  
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Set up as opções do devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
