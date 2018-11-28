774610abed3c569babec71a5f2ae9e1a57516740
Frederico Dib <fredericodib@cjr.org.br>
2018-11-28 12:35:31 -0200

    funcionando cadastro

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2018_11_27_142603) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
