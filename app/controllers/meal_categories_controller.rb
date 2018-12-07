945cbe208b2d12fe8d39c38d58d565647301c6cb
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2018-12-07 14:10:35 -0200

    backend do crud de categorias completo

--- app/controllers/meal_categories_controller.rb
+++ app/controllers/meal_categories_controller.rb
@@ -1,4 +1,6 @@
 class MealCategoriesController < ApplicationController
+  before_action :set_meal_category, only: [:destroy, :edit, :update]
+
   def index
     @meal_categories = MealCategory.all
   end
@@ -13,12 +15,25 @@ class MealCategoriesController < ApplicationController
   end
 
   def destroy
-    @meal_category = MealCategory.find(params[:id])
     @meal_category.destroy
     redirect_to meal_categories_url
-  end 
+  end
+
+  def edit 
+  end
+
+  def update
+    if @meal_category.update(meal_category_params)
+      redirect_to meal_categories_path 
+    else
+      render :edit 
+    end
+  end
 
   private
+  def set_meal_category
+    @meal_category = MealCategory.find(params[:id])
+  end
   def meal_category_params
     params.require(:meal_category).permit(:name)
   end
