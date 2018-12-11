f87b74dc268cceca533289505781e3f555cfab90
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2018-12-11 15:20:34 -0200

    trabalhei no meals#index. quase pronto

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,4 +1,5 @@
 class MealsController < ApplicationController
+  before_action :set_meal, only: [:destroy, :edit, :update]
 
   def new
     @meal = Meal.new
@@ -14,8 +15,27 @@ class MealsController < ApplicationController
     @meals = Meal.all
   end
 
-  def meal_params
-    params.require(:meal).permit(:name, :meal_category_id, :description, :price, :image)
+  def destroy
+    @meal.destroy
+    redirect_to meals_url
   end
-  
+
+  def edit 
+  end
+
+  def update
+    if @meals.update(meals_params)
+      redirect_to meals_path 
+    else
+      render :edit 
+    end
+  end
+
+  private
+  def set_meal
+    @meal = Meal.find(params[:id])
+  end
+  def meal_params
+    params.require(:meal).permit(:name, :meals_id, :description, :price, :image)
+  end
 end
