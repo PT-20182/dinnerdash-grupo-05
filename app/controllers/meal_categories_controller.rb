05b76ef89a9b55d711dcd6411afd8983663a6c49
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2018-12-13 10:16:34 -0200

    validação tá funcionando eu acho?

--- app/controllers/meal_categories_controller.rb
+++ app/controllers/meal_categories_controller.rb
@@ -1,5 +1,6 @@
 class MealCategoriesController < ApplicationController
   before_action :set_meal_category, only: [:destroy, :edit, :update]
+  before_action :require_admin_user
 
   def index
     @meal_categories = MealCategory.all
@@ -36,5 +37,10 @@ class MealCategoriesController < ApplicationController
   end
   def meal_category_params
     params.require(:meal_category).permit(:name)
+  end
+  def require_admin_user
+    unless current_user && current_user.is_admin
+      redirect_to new_user_session_path
+    end
   end
 end
